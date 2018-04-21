731d8048104df695f20315fd02ef8c49eed57f06
Pawel Bus <pawelbus.dev@outlook.com>
2018-04-21 14:31:06 +0200

    Add index.html and script.js files

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var a = 2,
+    h = 3,
+    triangleArea;
+
+triangleArea = a * h / 2;
+
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+alert('Give your numbers.');
+
+a = prompt('Base (a) = ?');
+h = prompt('Height (h) = ?');
+
+triangleArea = a * h / 2;
+
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+console.log('Dzięki funkcji "console.log" można wyświetlać komunikaty w konsoli :)');
